d1ec404312d45a57dc58f693bec0af3c410a7a79
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-02-02 03:45:15 +0300

    added burger builder project

---
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Aux from '../Aux/Aux';
+import Modal from '../../components/UI/Modal/Modal';
+import useHttpErrorHandler from '../../hoc/hooks/http-error-handler';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return props => {
+    const [error, clearError] = useHttpErrorHandler(axios);
+
+    return (
+      <Aux>
+        <Modal show={error}
+          modalClosed={clearError}>
+          {error ? error.message : null}
+        </Modal>
+        <WrappedComponent {...props} />
+      </Aux>
+    );
+  }
+}
+
+export default withErrorHandler;

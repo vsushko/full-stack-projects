48a1c6b5284af76be5f24d731016ad6f1b918fbb
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2023-02-06 03:45:00 +0300

    renamed project

---
+++ ppmtool/src/main/resources/static/precache-manifest.b8307b719544f4736bf0859c03355a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8438969d43a085794dc5",
+    "url": "/static/js/main.8438969d.chunk.js"
+  },
+  {
+    "revision": "8e4404cba1ce4f9ca71b",
+    "url": "/static/js/2.8e4404cb.chunk.js"
+  },
+  {
+    "revision": "8438969d43a085794dc5",
+    "url": "/static/css/main.22d11ef2.chunk.css"
+  },
+  {
+    "revision": "8e4404cba1ce4f9ca71b",
+    "url": "/static/css/2.77d57f52.chunk.css"
+  },
+  {
+    "revision": "234a8d34ebdc396c2d0581d8b2659657",
+    "url": "/index.html"
+  }
+];
